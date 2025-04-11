89312cfc550ad6628d66b04a0e4707d231704a3f
Hu Kaixiang <maxhu787@gmail.com>
2025-04-11 09:30:20 +0800

    setup themecontext

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,21 +1,24 @@
 import { Stack } from "expo-router";
 import { UserProvider } from "../contexts/UserContext";
 import { ProfileProvider } from "../contexts/ProfileContext";
+import { ThemeProvider } from "../contexts/ThemeContext";
 import { StatusBar } from "expo-status-bar";
 
 export default Layout = () => {
   return (
     <UserProvider>
       <ProfileProvider>
-        <StatusBar style="auto" />
-        <Stack
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="(app)" />
-          <Stack.Screen name="(auth)/signout" />
-        </Stack>
+        <ThemeProvider>
+          <StatusBar style="auto" />
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="(app)" />
+            <Stack.Screen name="(auth)/signout" />
+          </Stack>
+        </ThemeProvider>
       </ProfileProvider>
     </UserProvider>
   );
